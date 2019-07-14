5dcf94f683bc562093737a89b82e0f2c41e6b6cd
nirmic <nirmic@outlook.com>
2019-07-13 23:59:18 -0700

    push to github

--- push2github.bat
+++ push2github.bat
@@ -1,6 +1,10 @@
 rem for first time computer setup - need to add those 2 lines first dsf
 git init
-git remote add origin https://github.com/nirmic/projects.git
+rem account user:nirmic (password regular)
+rem git remote add origin https://github.com/nirmic/projects.git
+
+rem account user:nirmic75 (password regular)
+git remote add origin https://github.com/nirmic75/cwkprojects.git
 git add --all
 git commit -am "push to github"
 git push origin master --force
